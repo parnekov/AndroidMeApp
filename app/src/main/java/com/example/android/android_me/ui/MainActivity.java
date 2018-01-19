0cd2f26b1d8302f401f26b9fd4451ee070d3228b
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-01-19 15:15:38 +0200

    add sw600dp layout for tablets and add code in MainActivity for two panels

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.android.android_me.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnItemImageClickListener {
 
@@ -15,10 +18,50 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     private int bodyIndex;
     private int legsIndex;
 
+    private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.android_me_linear_layout) != null) {
+            mTwoPane = true;
+
+            Button nextButton = (Button) findViewById(R.id.button_new_activity);
+            nextButton.setVisibility(View.GONE);
+
+            GridView gridView = (GridView) findViewById(R.id.images_grid_view);
+            gridView.setNumColumns(2);
+
+            if (savedInstanceState == null) {
+                // create fragment object
+                BodyItemFragment bodyHeadFragment = new BodyItemFragment();
+                // set image
+                bodyHeadFragment.setIntegerList(AndroidImageAssets.getHeads());
+                bodyHeadFragment.setIntIndex(1);
+                // add the fragment to screen
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                // fragment transaction
+                fragmentManager.beginTransaction()
+                        .add(R.id.container_for_head, bodyHeadFragment)
+                        .commit();
+
+                BodyItemFragment bodyFragment = new BodyItemFragment();
+                bodyFragment.setIntegerList(AndroidImageAssets.getBodies());
+                fragmentManager.beginTransaction()
+                        .add(R.id.container_for_body, bodyFragment)
+                        .commit();
+
+                BodyItemFragment legFragment = new BodyItemFragment();
+                legFragment.setIntegerList(AndroidImageAssets.getLegs());
+                fragmentManager.beginTransaction()
+                        .add(R.id.container_for_legs, legFragment)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
 
     @Override
