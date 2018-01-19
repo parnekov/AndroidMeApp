4474c1ae27ef7e846e532b8b5b1dc3bd1aff51db
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-01-19 13:48:54 +0200

    add fragment_master_list.xml and turn on GridView

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,33 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+
+public class MasterListFragment extends Fragment {
+    public MasterListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        // Get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = (GridView) view.findViewById(R.id.images_grid_view);
+        // Create the adapter
+        MasterListAdapter adapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        // Set the adapter on the GridView
+        gridView.setAdapter(adapter);
+
+        return view;
+    }
+}
