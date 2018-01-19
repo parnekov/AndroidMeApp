e0facc8148523e95e1175fd479029330988fe0f1
Sasha Parnekov <oleksandr.parnekov@gmail.com>
2018-01-19 14:16:04 +0200

    define interface in MainActivity and create Toast

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnItemImageClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemClickImage(int position) {
+        Toast.makeText(this, "Position " + position, Toast.LENGTH_SHORT).show();
     }
 }
